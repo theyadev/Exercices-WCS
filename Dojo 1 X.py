83d7164b17a841c9b1f6d0a358c9576ca258519d
Theya <gtheya@gmail.com>
2021-09-30 18:42:41 +0200

    Peut-etre j'abuse, je sais pas

--- Dojo 1 X.py
+++ Dojo 1 X.py
@@ -1,8 +1,4 @@
 from random import randint
-
 def main(peoples):
-    for i in range(len(peoples)):
-        print(f'La {f"{i+1}ère" if i == 1 else "" f"{i+1}ème" if len(peoples) != 0 else "dernière"} personne à sortir est {peoples.pop(randint(0, len(peoples) - 1))}. {f"Il reste {len(peoples)} personnes !" if len(peoples) != 0 else ""}')
-
-if __name__ == "__main__":
-    main(["Quentin", "Adrien", "Jojo", "Jordan", "David", "Timothée", "Cécilia", "Théo", "Nicolas", "Gael"])
+    for i in range(len(peoples)): print(f'La {f"{i+1}ère" if i == 0 else "" f"{i+1}ème" if len(peoples) != 0 else "dernière"} personne à sortir est {peoples.pop(randint(0, len(peoples) - 1))}. {f"Il reste {len(peoples)} personnes !" if len(peoples) != 0 else ""}')
+main(["Quentin", "Adrien", "Jojo", "Jordan", "David", "Timothée", "Cécilia", "Théo", "Nicolas", "Gael"])
