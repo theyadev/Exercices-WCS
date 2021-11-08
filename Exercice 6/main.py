b9490e18b7cab38c8ed6dd475a6a1a319fd0400a
theyadev <gtheya@gmail.com>
2021-11-08 10:09:33 +0100

    removed class animalslist

--- Exercice 6/main.py
+++ Exercice 6/main.py
@@ -16,7 +16,7 @@ import random
 # 4 - A chaque étape de la course, afficher la liste des animaux (et s'il est arrivé) sous la forme :
 #     {Nom}, le {Espèce} {Couleur}, a parcouru {Distance}km (et est arrivé en position {Position})
 
-# 5 - A la fin de la course 
+# 5 - A la fin de la course
 #     (lorsque tous les animaux ont franchi la ligne d'arrivée)
 #     Afficher le résultat sous la forme :
 #     La course a durée {heures} heures, le podium est :
@@ -24,12 +24,34 @@ import random
 
 #     La liste doit être triée par ordre d'arrivée
 
-class AnimalsList:
-    def __init__(self):
-        self.animals = []
 
-    def printAnimals(self):
-        n = len(self.animals)
+class Animal:
+    lst = []
+
+    def __init__(
+        self,
+        nb: int = None,
+        family: str = None,
+        color: str = None,
+        max_speed: int = 20,
+        age: int = 1,
+        name: str = None
+    ) -> None:
+        self.name = name
+        self.nb = len(self.lst) + 1
+        self.family = family
+        self.color = color
+        self.max_speed = max_speed
+        self.age = age
+
+        self.lst.append(self)
+
+    def printAnimal(self):
+        return f"{self.name} {self.nb} {self.color} a {self.age} an{'s' if self.age > 1 else ''}"
+
+    @classmethod
+    def printAnimals(cls):
+        n = len(cls.lst)
 
         if n == 0:
             return "Il n'y a pas d'animaux dans la liste ! --'"
@@ -41,54 +63,34 @@ class AnimalsList:
         else:
             text += "Le seul animal de la liste :) :\n"
 
-        for animal in self.animals:
+        for animal in cls.lst:
             text += animal.printAnimal() + "\n"
 
         return text
 
-    def addAnimal(self, animal):
-        index = len(self.animals) + 1
-        animal.nb = index
-        self.animals.append(animal)
+    @classmethod
+    def hasName(cls, string: str):
+        return True if any(animal for animal in cls.lst if animal.name == string.capitalize()) else False
 
-    def hasName(self, string: str):
-        return True if any(animal for animal in self.animals if animal.name == string.capitalize()) else False
+    @classmethod
+    def oldAnimals(cls, years: int, family: str = None, color: str = None, name: str = None):
+        filtered_animals: list[Animal] = cls.lst
 
-    def oldAnimals(self, years: int, family: str = None, color: str = None, name: str = None):
-        filtered_animals: list[Animal] = self.animals
-        
         if family is not None:
-            filtered_animals: list[Animal] = list(filter(lambda x: x.family == family, filtered_animals))
+            filtered_animals: list[Animal] = list(
+                filter(lambda x: x.family == family, filtered_animals))
 
         if color is not None:
-            filtered_animals: list[Animal] = list(filter(lambda x: x.color == color, filtered_animals))
+            filtered_animals: list[Animal] = list(
+                filter(lambda x: x.color == color, filtered_animals))
 
         if name is not None:
-            filtered_animals: list[Animal] = list(filter(lambda x: x.name == name, filtered_animals))
+            filtered_animals: list[Animal] = list(
+                filter(lambda x: x.name == name, filtered_animals))
 
         for animal in filtered_animals:
             animal.age += years
-                     
 
-class Animal:
-    def __init__(
-        self, 
-        nb: int = None, 
-        family: str = None, 
-        color: str = None, 
-        max_speed: int = 20, 
-        age: int = 1,
-        name: str = None
-        ) -> None:
-        self.name = name
-        self.nb = nb
-        self.family = family
-        self.color = color
-        self.max_speed = max_speed
-        self.age = age
-        
-    def printAnimal(self):
-        return f"{self.name} {self.nb} {self.color} a {self.age} an{'s' if self.age > 1 else ''}"
 
 def readJSON(path):
     try:
@@ -97,10 +99,13 @@ def readJSON(path):
     except:
         return []
 
+
 types_animals = readJSON("./animaux.json")
 
-family_list = ["Lapin", "Chat", "Mésange", "Python", "Chien", "Tourterelle", "Coccinelle", "Abeille"]
+family_list = ["Lapin", "Chat", "Mésange", "Python",
+               "Chien", "Tourterelle", "Coccinelle", "Abeille"]
 colors_list = ["Blanc", "Vert", "Bleu", "Rouge", "Jaune"]
+
 
 def main():
     nb_animaux = input("Combien d'animaux voulez-vous faire naître ? : ")
@@ -108,9 +113,7 @@ def main():
     if not nb_animaux.isnumeric():
         print("Vous n'avez pas rentré un nombre... --'")
         return
-        
-    animals = AnimalsList()
-    
+
     nb_animaux = int(nb_animaux)
 
     for i in range(nb_animaux):
@@ -121,17 +124,18 @@ def main():
         name = family
 
         if len(family_dict['names']) > 0:
-            name = family_dict['names'].pop(floor(random.random() * len(family_dict['names'])))
-            
+            name = family_dict['names'].pop(
+                floor(random.random() * len(family_dict['names'])))
 
-        animals.addAnimal(Animal(family=family, color=choice(family_dict['colors']), name=name, max_speed=family_dict['max_speed']))
-    print(animals.printAnimals())
+        Animal(family=family, color=choice(
+            family_dict['colors']), name=name, max_speed=family_dict['max_speed'])
+    print(Animal.printAnimals())
+    years_old = None
 
-    years_old = 1000000
-
-    while years_old > 0:
+    while years_old is None or years_old > 0:
         criteria = input("Quelles animaux voulez-vous faire vieillir ? : ")
-        years_old = input("De combien d'années souhaitez-vous faire vieillir les animaux ? : ") 
+        years_old = input(
+            "De combien d'années souhaitez-vous faire vieillir les animaux ? : ")
 
         if not years_old.isnumeric():
             print("L'age doit être un nombre !")
@@ -150,26 +154,25 @@ def main():
             if arg.capitalize() in family_list:
                 family = arg.capitalize()
                 continue
-                
+
             if arg.capitalize() in colors_list:
                 color = arg.capitalize()
                 continue
 
-            if animals.hasName(arg):
+            if Animal.hasName(arg):
                 name = arg.capitalize()
                 continue
 
-        animals.oldAnimals(years_old, family, color, name)
+        print(color, family, name)
+        Animal.oldAnimals(years_old, family, color, name)
 
-        print(animals.printAnimals())
+        print(Animal.printAnimals())
 
     length_race = ""
 
     while not length_race.isnumeric() and length_race:
         input("Quel sera la taille de la course ? ")
-    
 
-    
 
 if __name__ == "__main__":
     main()
